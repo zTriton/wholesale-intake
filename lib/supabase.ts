2280387905a81fec8e235f2f06ed95d697412181
zTriton <50779908+zTriton@users.noreply.github.com>
2025-08-24 16:38:15 -0400

    add supabase client

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+export const supa = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE!,
+  { auth: { persistSession: false } }
+);
